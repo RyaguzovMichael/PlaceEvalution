5c7009190338cf6d99f384930ab05170add36f64
miha <miha890r@gmail.com>
2022-12-10 18:58:21 +0600

    Упаковка в Docker проекта (feature) - Удаление из проекта FrontEnd - Добавление файла Readme.md

--- Frontend/src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import Notifications from '@kyvg/vue3-notification'
-
-createApp(App).use(router).use(Notifications).mount('#app')
